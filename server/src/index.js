a94628efd044d110733676cfaca4189c50494d51
Alessander Franca <alessander70@gmail.com>
2018-06-02 11:00:37 -0300

    mongo context added to graphql server

--- server/src/index.js
+++ server/src/index.js
@@ -2,13 +2,19 @@ import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import {graphqlExpress, graphiqlExpress} from 'apollo-server-express';
+import connectMongo from './db/mongo-connector';
 import schema from './schemas/index';
 
 const start = async () => {
+  const mongo = await connectMongo();
   const PORT = 4000;
   const app = express();
   app.use(cors());
-  app.use('/graphql', bodyParser.json(), graphqlExpress({schema}));
+  app.use(
+    '/graphql',
+    bodyParser.json(),
+    graphqlExpress({schema, context: {mongo}}),
+  );
   // if you want GraphiQL enabled - disable at production
   app.get('/graphiql', graphiqlExpress({endpointURL: '/graphql'}));
 
