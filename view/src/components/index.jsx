a190d29408819792a856a357948b7788664e73e3
Alessander Franca <alessander70@gmail.com>
2018-06-02 12:21:26 -0300

    view setup

---
+++ view/src/components/index.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import ApolloClient, { InMemoryCache } from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+import App from './App';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  cache: new InMemoryCache(),
+});
+
+const render = (Component) => {
+  ReactDOM.render(
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Component />
+      </BrowserRouter>
+    </ApolloProvider>,
+    document.getElementById('root'),
+  );
+};
+
+render(App);
+
+if (process.env.NODE_ENV === 'development') {
+  module.hot.accept('./App', () => {
+    render(App);
+  });
+}
