9735fa9068c01eb4e6712b24a19770fc3e707a4d
Alessander Franca <alessander70@gmail.com>
2018-06-02 10:45:44 -0300

    docker-compose setup for node server and graphql server running or port 4000

--- server/src/index.js
+++ server/src/index.js
@@ -5,13 +5,10 @@ import {graphqlExpress, graphiqlExpress} from 'apollo-server-express';
 import schema from './schemas/index';
 
 const start = async () => {
+  const PORT = 4000;
   const app = express();
   app.use(cors());
-  app.use(
-    '/graphql',
-    bodyParser.json(),
-    graphqlExpress({schema, context: {mongo}}),
-  );
+  app.use('/graphql', bodyParser.json(), graphqlExpress({schema}));
   // if you want GraphiQL enabled - disable at production
   app.get('/graphiql', graphiqlExpress({endpointURL: '/graphql'}));
 
