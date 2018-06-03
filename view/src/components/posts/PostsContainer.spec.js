570136a3acf483c83cddee36bdecc5a03a845f1f
Alessander Franca <alessander70@gmail.com>
2018-06-03 12:39:18 -0300

    react-test-renderer switched to react-testing-library

--- view/src/components/posts/PostsContainer.spec.js
+++ view/src/components/posts/PostsContainer.spec.js
@@ -3,39 +3,41 @@ import { InMemoryCache } from 'apollo-boost';
 import { ApolloClient } from 'apollo-client';
 import { ApolloProvider, graphql } from 'react-apollo';
 import { createHttpLink } from 'apollo-link-http';
-import renderer from 'react-test-renderer';
 import MockNetworkInterface from 'apollo-mocknetworkinterface';
+import { renderIntoDocument, cleanup } from 'react-testing-library';
 import query from '../../utils/queries';
 import PostsContainer from './PostsContainer';
 
 const TestComponentWithApollo = graphql(query)(PostsContainer);
 
-const createResponse = (request) => {
-  // pure function returning data
-  console.log('creating mocked response for request:', request); // eslint-disable-line no-console
-  // will log: creating mocked response for request: { query: { kind: 'Document' ...
-  return {
-    data: {
-      allPosts: [
-        {
-          id: '1', title: 'title', description: 'description', __typename: '',
-        },
-      ],
-    },
-  };
-};
+const createResponse = () => ({
+  data: {
+    allPosts: [
+      {
+        id: '1',
+        title: 'title',
+        description: 'description',
+        __typename: '',
+      },
+    ],
+  },
+});
 const mockedNetworkFetch = MockNetworkInterface(createResponse, { timeout: 100 });
 const client = new ApolloClient({
   link: createHttpLink({ uri: 'http://localhost:4000', fetch: mockedNetworkFetch }),
   cache: new InMemoryCache(),
 });
 
+afterEach(cleanup);
+
 it('should render without exploding', (done) => {
-  const component = renderer.create(
+  const component = (
     <ApolloProvider client={client}>
       <div>
         <TestComponentWithApollo />
       </div>
-    </ApolloProvider>);
+    </ApolloProvider>
+  );
+  renderIntoDocument(component);
   done();
 });
